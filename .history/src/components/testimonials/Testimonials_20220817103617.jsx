dadf4a6a6b73a71e8fffead5ddc7d210662c83c6
Daniel Motta <mottad14@gmail.com>
2022-08-17 22:06:55 -0500

    Completed all sections, now editing content

---
+++ .history/src/components/testimonials/Testimonials_20220817103617.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './testimonials.css'
+//Import all the images here ()
+
+import {Navigation, Pagination, Scrollbar, Ally} from "swiper";
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+
+
+const Testimonials = () => {
+  return (
+    <div>Testimonials</div>
+  )
+}
+
+export default Testimonials
---
+++ .history/src/components/testimonials/Testimonials_20220817103645.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './testimonials.css'
+//Import all the images here ()
+
+import {Navigation, Pagination, Scrollbar, Ally} from "swiper";
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/css';
+import 'swiper/css/pagination'
+
+
+const Testimonials = () => {
+  return (
+    <div>Testimonials</div>
+  )
+}
+
+export default Testimonials
